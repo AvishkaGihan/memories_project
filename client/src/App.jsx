0814ac4e2f92e67bd0a434e4d0d3d0cdc832a995
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2023-12-30 21:28:21 +0530

    Add dependencies and update styling in client package.json and index.html, and update App.jsx with custom components and layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,43 @@
+import { Container, Grow, Grid } from "@mui/material";
+
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import {
+  StyledCustomAppBar,
+  StyledHeading,
+  StyledImage,
+} from "./StyledComponents"; // Importing custom components
+
+import memories from "./images/memories.png";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Container maxWidth="lg">
+      <StyledCustomAppBar position="static" color="inherit">
+        <StyledHeading variant="h2" align="center">
+          Memories
+        </StyledHeading>
+        <StyledImage src={memories} alt="memories" height="60" />
+      </StyledCustomAppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={4}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 };
 
 export default App;
